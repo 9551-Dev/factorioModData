6f5740b5a3979438b024058fb6d6a376656a0d36
9551 <74534592+9551-Dev@users.noreply.github.com>
2022-01-16 19:57:27 +0100

    fixed other mod support

--- rangemodifier_0.0.1/data-final-fixes.lua
+++ rangemodifier_0.0.1/data-final-fixes.lua
@@ -6,7 +6,7 @@ for k,v in pairs(data.raw["roboport"]) do
 end
 local mult = settings.startup["cabrangemult"].value
 for k,v in pairs(data.raw["electric-pole"]) do
-    local wireReach,supplyArea = math.min(v.maximum_wire_distance*mult,64),v.supply_area_distance*mult
+    local wireReach,supplyArea = math.min(v.maximum_wire_distance*mult,64),math.min(v.supply_area_distance*mult,64)
     data.raw["electric-pole"][k].maximum_wire_distance = wireReach
     data.raw["electric-pole"][k].supply_area_distance = supplyArea
 end
