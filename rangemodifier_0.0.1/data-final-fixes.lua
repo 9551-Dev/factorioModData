d851a1e7be8d8c365a487388082dec951769d7b5
9551 <74534592+9551-Dev@users.noreply.github.com>
2022-01-16 18:55:57 +0100

    Add files via upload

---
+++ rangemodifier_0.0.1/data-final-fixes.lua
@@ -0,0 +1,12 @@
+local mult = settings.startup["robrangemult"].value
+for k,v in pairs(data.raw["roboport"]) do
+    local logiRad,consRad = v.logistics_radius*mult,v.construction_radius*mult
+    data.raw["roboport"][k].construction_radius = consRad
+    data.raw["roboport"][k].logistics_radius = logiRad
+end
+local mult = settings.startup["cabrangemult"].value
+for k,v in pairs(data.raw["electric-pole"]) do
+    local wireReach,supplyArea = math.min(v.maximum_wire_distance*mult,64),v.supply_area_distance*mult
+    data.raw["electric-pole"][k].maximum_wire_distance = wireReach
+    data.raw["electric-pole"][k].supply_area_distance = supplyArea
+end
---
+++ rangemodifier_0.0.1/settings.lua
@@ -0,0 +1,18 @@
+data:extend({
+    {
+        type = "int-setting",
+        name = "cabrangemult",
+        setting_type = "startup",
+        default_value = 2,
+        minimum_value = 1,
+        maximum_value = 5
+    },
+    {
+        type = "int-setting",
+        name = "robrangemult",
+        setting_type = "startup",
+        default_value = 2,
+        minimum_value = 1,
+        maximum_value = 5
+    }
+})
