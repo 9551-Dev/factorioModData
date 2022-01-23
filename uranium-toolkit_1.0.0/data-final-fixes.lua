95a5be10cd19cf21ed21675d3988bc7d90cd4a34
9551 <74534592+9551-Dev@users.noreply.github.com>
2022-01-23 16:47:41 +0100

    Delete uranium-toolkit_1.0.0 directory

--- uranium-toolkit_1.0.0/data-final-fixes.lua
+++
@@ -1,4 +0,0 @@
-data.raw["transport-belt"]["express-transport-belt"].next_upgrade = "uranium-belt"
-data.raw["underground-belt"]["express-underground-belt"].next_upgrade = "uranium-underground-belt"
-data.raw["splitter"]["express-splitter"].next_upgrade = "uranium-splitter"
-data.raw["inserter"]["stack-inserter"].next_upgrade = "uranium-inserter"
--- uranium-toolkit_1.0.0/data-updates.lua
+++
@@ -1,4 +0,0 @@
-require("__uranium-toolkit__/prototypes/entities/uranium-belt")
-require("__uranium-toolkit__/prototypes/entities/uranium-underground")
-require("__uranium-toolkit__/prototypes/entities/uranium-splitter")
-require("__uranium-toolkit__/prototypes/entities/uranium-inserter")
