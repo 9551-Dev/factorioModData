e17707b758d2a93f4cdd9c5d4d9640f198acb36b
9551 <74534592+9551-Dev@users.noreply.github.com>
2022-01-30 10:03:55 +0100

    Add files via upload

---
+++ HSV-lamps_1.1.0/prototypes/items/flat-hsv-lamp.lua
@@ -0,0 +1,12 @@
+data:extend({
+    {
+        type="item",
+        name="small-flat-hsv-lamp-item",
+        stack_size=50,
+        icon="__HSV-lamps__/graphics/icons/hsv-flat-lamp-icon.png",
+        icon_size=32,
+        place_result="small-flat-hsv-lamp",
+        localised_name="HSV lamp",
+        localised_description="a colorful lamp with HSV support"
+    }
+})
