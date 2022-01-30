90ae45978228ba5476cd779de9fe5df272e220cf
9551 <74534592+9551-Dev@users.noreply.github.com>
2022-01-30 21:15:55 +0100

    Delete HSV-lamps_1.2.0 directory

--- HSV-lamps_1.2.0/prototypes/signals/virtual-val.lua
+++
@@ -1,9 +0,0 @@
-data:extend({
-    {
-        type="virtual-signal",
-        localised_name="[HSV lamps] VALUE",
-        name="hsv-val",
-        icon="__HSV-lamps__/graphics/signals/val.png",
-        icon_size=32
-    }
-})
