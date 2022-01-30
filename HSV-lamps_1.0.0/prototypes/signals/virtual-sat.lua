01c0f162f12c12f9f69a2cf3869ffd736721ba25
9551 <74534592+9551-Dev@users.noreply.github.com>
2022-01-30 10:02:55 +0100

    Delete HSV-lamps_1.0.0 directory

--- HSV-lamps_1.0.0/prototypes/signals/virtual-sat.lua
+++
@@ -1,9 +0,0 @@
-data:extend({
-    {
-        type="virtual-signal",
-        localised_name="[HSV lamps] SATURATION",
-        name="hsv-sat",
-        icon="__HSV-lamps__/graphics/signals/sat.png",
-        icon_size=32
-    }
-})
