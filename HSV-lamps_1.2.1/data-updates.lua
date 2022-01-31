e48c8f81a3243c9658dd6174506093b0642c9714
9551 <74534592+9551-Dev@users.noreply.github.com>
2022-01-31 17:24:59 +0100

    Delete HSV-lamps_1.2.1 directory

--- HSV-lamps_1.2.1/data-updates.lua
+++
@@ -1,4 +0,0 @@
-require("__HSV-lamps__/prototypes/science/research.lua")
-
-require("__HSV-lamps__/prototypes/lamps/small-hsv-lamp.lua")
-require("__HSV-lamps__/prototypes/lamps/flat-lamp/small-flat-hsv-lamp.lua")
--- HSV-lamps_1.2.1/prototypes/signals/virtual-hue.lua
+++
@@ -1,9 +0,0 @@
-data:extend({
-    {
-        type="virtual-signal",
-        localised_name="[HSV lamps] HUE",
-        name="hsv-hue",
-        icon="__HSV-lamps__/graphics/signals/hue.png",
-        icon_size=32
-    }
-})
