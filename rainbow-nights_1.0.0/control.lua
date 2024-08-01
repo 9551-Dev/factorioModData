0c2e4cfc59c00550cbe806144ea1fe018682b1c6
9551-Dev <74534592+9551-Dev@users.noreply.github.com>
2024-08-01 04:38:22 +0200

    What fucking retard wrote this

--- rainbow-nights_1.0.0/control.lua
+++ rainbow-nights_1.0.0/control.lua
@@ -1,9 +1,9 @@
 local HSVtoRGB = require("__rainbow-nights__/api").HSV
 
 local smoothness = settings.startup["smoothness"].value
-local n = math.floor(255 - (settings.startup["visibility"].value*2.55))/255
+local n = math.floor(360 - (settings.startup["visibility"].value*3.66))/360
 script.on_nth_tick(settings.startup["updaterate"].value,function(event)
-    local tick = ((event.tick/settings.startup["timedivider"].value) % (255*smoothness))/smoothness
+    local tick = ((event.tick/settings.startup["timedivider"].value) % (360*smoothness))/smoothness
     local r,g,b = HSVtoRGB(tick,settings.startup["SAT"].value,settings.startup["VALUE"].value)
     r,g,b = math.max(0,r-n),math.max(0,g-n),math.max(0,b-n)
     for k,v in pairs(game.surfaces) do
